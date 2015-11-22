bb40f93f9a28a299cc042d4a92b61b9165d633fc
dreammis <hysn2001@gmail.com>
2015-11-22 16:20:49 +0800

    send mail sussess and finish the register

--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+import ConfigParser
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = 'smtp.sina.com'
-    MAIL_PORT = 465
+    # SMTP_SERVER = 465
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USE_SSL = False
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    #read the configInfo from ini files
+    configParser = ConfigParser.ConfigParser()
+    configParser.read('config.ini')
+    MAIL_USERNAME = configParser.get('mailInfo','user')
+    MAIL_PASSWORD = configParser.get('mailInfo','password')
+    
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <firedirx@sina.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <%s>'%MAIL_USERNAME
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ from app import create_app, db
 from app.models import User, Role
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
