67ca6b99d4011f9eb388a1ee847da6915541edc1
dreammis <hysn2001@gmail.com>
2015-11-23 23:41:07 +0800

    a

--- config.py
+++ config.py
@@ -20,7 +20,8 @@ class Config:
 
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <%s>'%MAIL_USERNAME
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = MAIL_USERNAME
+    # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
     def init_app(app):
