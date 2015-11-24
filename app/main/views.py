9e3b4691e80c876b5137fc4762fee9f3bbbbcbe2
dreammis <hysn2001@gmail.com>
2015-11-25 01:49:56 +0800

    添加了post页面的逻辑处理

--- app/main/views.py
+++ app/main/views.py
@@ -28,24 +28,33 @@
 
 from flask import render_template
 from . import main
-from .. import db
-from ..models import User,Role
+from .. import db,cache
+from ..models import User,Role,Permission,Post
 from flask import abort,flash,redirect,url_for
 from flask.ext.login import login_required,current_user
-from .forms import EditProfileForm,EditProfileAdminForm
+from .forms import EditProfileForm,EditProfileAdminForm,PostForm
 from ..decorators import admin_required
 
 
-@main.route('/')
+@main.route('/',methods=['GET','POST'])
+@cache.cached(timeout=50)
 def index():
-    return render_template('index.html')
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+        form.validate_on_submit():
+        post = Post(body=form.body.data,author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html',form=form,posts=posts)
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    return render_template('user.html',user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html',user=user,posts=posts)
 
 @main.route('/edit-profile',methods=['POST','GET'])
 @login_required
@@ -89,3 +98,4 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html',form=form,user=user)
+
