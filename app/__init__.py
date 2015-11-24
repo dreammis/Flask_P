45763b7906e3a1da8297a2b257bacd9ee9bc8af5
dreammis <hysn2001@gmail.com>
2015-11-25 00:16:08 +0800

    add cache but i don't know how to use it

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
+from flask.ext.cache import Cache
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+cache  = Cache()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -24,6 +26,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    cache.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
