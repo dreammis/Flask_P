33512b913da7b7d335adec7463063704e3b96abc
dreammis <hysn2001@gmail.com>
2015-11-26 02:34:55 +0800

    为响应新加模块添加配置项

--- config.py
+++ config.py
@@ -24,6 +24,8 @@ class Config:
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     #add by 2015/11/25
     FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 20
+    FLASKY_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User, Role,Permission,Post
+from app.models import User, Role,Permission,Post,Follow,Comment
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 import sys
@@ -13,7 +13,8 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission,Post=Post)
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission,Post=Post,Follow=Follow,
+                Comment=Comment)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
